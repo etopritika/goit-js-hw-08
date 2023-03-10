57a61f5b4c50573354ba7db783a0f4163c47f9b5
etopritika <dimaprytyka@gmail.com>
2023-03-10 13:34:58 +0200

    init

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(iframe);
+console.log(player);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
