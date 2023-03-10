8fd67c1449172c98e0647227dea16845d0dbceb8
etopritika <dimaprytyka@gmail.com>
2023-03-10 16:27:39 +0200

    init

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,10 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const VIMEO_KEY_LS = 'videoplayer-current-time';
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const setWatchingTime = ({ seconds }) => {
-  localStorage.setItem(VIMEO_KEY_LS, seconds);
+  localStorage.setItem(STORAGE_KEY, seconds);
 };
 player.on('timeupdate', throttle(setWatchingTime, 1000));
-player.setCurrentTime(localStorage.getItem(VIMEO_KEY_LS) || 0);
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
