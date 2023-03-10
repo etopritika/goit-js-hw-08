61c2a0daeb63e72b09a3ecc3b3d817243243faa6
etopritika <dimaprytyka@gmail.com>
2023-03-10 14:39:08 +0200

    init

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,19 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const throttledOnLocalStorage = _.throttle(test, 1000);
+// const throttledOnLocalStorage = _.throttle(test, 1000);
 // console.log(iframe);
 // console.log(player);
 
-player.on('play', onPlay);
+player.on('timeupdate', onPlay);
 
 const onPlay = function (data) {
   localStorage.setItem('videoplayer-current-time', data);
-//   const currentTime = {
-//     duration: 61.857,
-//     percent: 0.049,
-//     seconds: 3.034,
-//   };
-//   player.setCurrentTime(currentTime);
+
 console.log(data);
 };
