5dc864c4c466ed099e3cabf3f26fd2f0aa0ac725
etopritika <dimaprytyka@gmail.com>
2023-03-10 14:20:47 +0200

    init

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,11 +6,17 @@ const player = new Player(iframe);
 // console.log(iframe);
 // console.log(player);
 
-player.on('timeupdate', onPlay);
+player.on('play', function () {
+  localStorage.setItem('videoplayer-current-time', data), 1000;
+});
 
-const onPlay = function(data) {
-    console.log('played the video!');
+const onPlay = function (data) {
+  localStorage.getItem('videoplayer-current-time');
+  const currentTime = {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+  };
+  player.setCurrentTime(currentTime);
 };
-
-
-
+player.on('play', onPlay);
